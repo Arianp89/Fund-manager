7021955aab24eb856efcc52354324dd7ebdfb256
Arian <arianpanahi6@gmail.com>
2026-06-25 18:39:22 +0330

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os 
+
+db_confing={'host': os.environ.get('db_host'),'user': os.environ.get('db_user'),'password': os.environ.get('password')} 
+database_name = os.environ.get('db_name') 
+
+
+API_TOKEN = os.environ.get('tel_token') 
+
+proxy_1=os.environ.get('proxy_1') 
+proxy_2=os.environ.get('proxy_2') 
+            
---
+++ main.py
@@ -0,0 +1,82 @@
+from config import API_TOKEN 
+from telebot.types import ReplyKeyboardMarkup, ReplyKeyboardRemove,InlineKeyboardMarkup, InlineKeyboardButton,KeyboardButton 
+import telebot 
+import logging
+from fun import *
+
+#____________________________________LOGG_______________________________________
+
+logging.basicConfig("level=logging.INFO, filename='project.log', format='%(asctime)s - %(levelname)s - %(message)s")
+
+#____________________________________MAKE-BOT____________________________________
+
+telebot.apihelper.API_URL = 'http://tapi.bale.ai/bot{0}/{1}' 
+bot=telebot.TeleBot(API_TOKEN)
+
+#____________________________________COMMAND_____________________________________
+
+
+
+
+
+
+
+
+
+
+#____________________________________BUTTON______________________________________
+
+
+
+
+
+
+
+
+
+
+#____________________________________LISENER_____________________________________
+
+def listener(messages):
+    for m in messages: 
+        # print(m) 
+        if m.content_type == "text": 
+            print(f"{m.chat.first_name} [{str(m.chat.id)}]: {m.text}") 
+            logging.info(f"{m.chat.first_name} [{str(m.chat.id)}]: {m.text}") 
+        elif m.content_type == "photo": 
+            print(f"{m.chat.first_name} [{str(m.chat.id)}]: New photo recieved") 
+            logging.info(f"{m.chat.first_name} [{str(m.chat.id)}]: New photo recieved") 
+        elif m.content_type == "document": 
+            print(f"{m.chat.first_name} [{str(m.chat.id)}]: New document recieved") 
+            logging.info(f"{m.chat.first_name} [{str(m.chat.id)}]: New document recieved") 
+        elif m.content_type == "voice":
+            print(f"{m.chat.first_name} [{str(m.chat.id)}]: New voice recieved") 
+            logging.info(f"{m.chat.first_name} [{str(m.chat.id)}]: New voice recieved") 
+bot.set_update_listener(listener) 
+            
+
+#____________________________CALLS_______________________
+
+@bot.callback_query_handler(func=lambda call: True)
+def all_callback_query_handler(call):
+    call_id = call.id
+    cid = call.message.chat.id
+    mid = call.message.message_id
+    data = call.data
+    print(f'call={call.message.from_user.first_name} [{cid}]:{data}')
+
+
+
+#________________________________ALL-MESSAGE__________________________
+@bot.message_handler(func=lambda message: True)
+def all_message_handler(message):
+    cid = message.chat.id
+
+
+print('code running...') 
+logging.info('code running...') 
+bot.infinity_polling() 
+            
+'code write it by:'
+'arian panahi  github id : http://github.com/arianp89'  
+            
