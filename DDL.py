5f271f411da376172f72b0bbeec6f95439e07e02
Arian <arianpanahi6@gmail.com>
2026-07-03 19:19:09 +0330

    update database

--- DDL.py
+++ DDL.py
@@ -15,6 +15,41 @@ def create_database(database_name):
     conn.close()
     print(f'database {database_name} created successfully')
 
+def create_table_setting(database_name):
+    conn=mysql.connector.connection.MySQLConnection(**db_config, database=database_name)
+    cur=conn.cursor()
+    SQL_Query="""
+    CREATE TABLE SETTING(
+    `CART_NUMBER`       BIGINT NOT NULL, 
+    `REGISTER_DATE`     DATETIME DEFAULT CURRENT_TIMESTAMP ,
+    `LAST_UPDATE`       DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+    """
+    cur.execute(SQL_Query)
+    conn.commit()
+    cur.close()
+    conn.close()
+    print(f'table setting created successfully')
+
+
+def create_table_family(database_name):
+    conn=mysql.connector.connection.MySQLConnection(**db_config, database=database_name)
+    cur=conn.cursor()
+    SQL_Query="""
+    CREATE TABLE FAMILY(
+    `ID`                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `FAMILY_NAME`       VARCHAR(20) NOT NULL,
+    `HEAD_ID`           BIGINT ,
+    `TOTAL_CAPITAL`     BIGINT NOT NULL ,
+    `REGISTER_DATE`     DATETIME DEFAULT CURRENT_TIMESTAMP ,
+    `LAST_UPDATE`       DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+    """
+    cur.execute(SQL_Query)
+    conn.commit()
+    cur.close()
+    conn.close()
+    print(f'table FAMILY created successfully')
 
 
 def create_table_customer(database_name):
@@ -23,12 +58,14 @@ def create_table_customer(database_name):
     SQL_Query="""
     CREATE TABLE CUSTOMER(
     `ID`                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `FAMILY_ID`         BIGINT UNSIGNED ,
     `BOT_ID`            BIGINT ,
-    `FIRST_NAME`        VARCHAR(15) ,
-    `LAST_NAME`         VARCHAR(15) ,
+    `FULL_NAME`         VARCHAR(15) ,
     `TOTAL_CAPITAL`     BIGINT NOT NULL ,
+    `IS_ACTIVE`         VARCHAR(5) ,
     `REGISTER_DATE`     DATETIME DEFAULT CURRENT_TIMESTAMP ,
-    `LAST_UPDATE`       DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `LAST_UPDATE`       DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+    FOREIGN KEY (FAMILY_ID) REFERENCES FAMILY(ID)
     );
     """
     cur.execute(SQL_Query)
@@ -44,8 +81,9 @@ def create_table_admin(database_name):
     cur=conn.cursor()
     SQL_Query="""
     CREATE TABLE ADMIN(
+    `ID`                    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     `CUSTOMER_ID`           BIGINT UNSIGNED NOT NULL ,
-    `ACCESS_LEVEL`          INT ,
+    `ACCESS_LEVEL`          INT NOT NULL,
     `REGISTER_DATE`         DATETIME DEFAULT CURRENT_TIMESTAMP ,
     `LAST_UPDATE`           DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
     FOREIGN KEY (CUSTOMER_ID) REFERENCES CUSTOMER(ID)
@@ -58,31 +96,13 @@ def create_table_admin(database_name):
 
 
 
-def create_table_supervisor(database_name):
-    conn=mysql.connector.connection.MySQLConnection(**db_config , database=database_name)
-    cur=conn.cursor()
-    SQL_Query="""
-    CREATE TABLE SUPERVISOR(
-    `CUSTOMER_ID`       BIGINT UNSIGNED NOT NULL ,
-    `LIST_SUPERVISOR`   TEXT ,
-    `REGEITER_DATE`     DATETIME DEFAULT CURRENT_TIMESTAMP ,
-    `LAST_UPDATE`       DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
-    FOREIGN KEY (CUSTOMER_ID) REFERENCES CUSTOMER(ID)
-    );
-    """
-    cur.execute(SQL_Query)
-    conn.commit()
-    cur.close()
-    conn.close()
-    print(f'table supervisor created successfully')
-
-
 
 def create_table_loan(database_name):
     conn=mysql.connector.connection.MySQLConnection(**db_config , database=database_name)
     cur=conn.cursor()
     SQL_Query="""
     CREATE TABLE LOAN(
+    `ID`                                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     `CUSTOMER_ID`                       BIGINT UNSIGNED NOT NULL ,
     `LOAN_AMOUNT`                       BIGINT NOT NULL ,
     `NUMBER_REMAINING_INSTALLMENTS`     INT , 
@@ -100,12 +120,38 @@ def create_table_loan(database_name):
 
 
 
+def create_table_installment(database_name):
+    conn=mysql.connector.connection.MySQLConnection(**db_config, database=database_name)
+    cur=conn.cursor()
+    SQL_Query="""
+    CREATE TABLE INSTALLMENT(
+    `ID`                    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `LOAN_ID`               BIGINT UNSIGNED ,
+    `INSTALLMENT_NUMBER`    INT ,
+    `PAID_DATA`             TEXT ,
+    `STATUS`                VARCHAR(5) ,
+    `TOTAL_CAPITAL`         BIGINT NOT NULL ,
+    `REGISTER_DATE`         DATETIME DEFAULT CURRENT_TIMESTAMP ,
+    `LAST_UPDATE`           DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+    FOREIGN KEY (LOAN_ID) REFERENCES LOAN(ID)
+    );
+    """
+    cur.execute(SQL_Query)
+    conn.commit()
+    cur.close()
+    conn.close()
+    print(f'table customer created successfully')
+
+
+
 
 
 if __name__ == '__main__':
     create_database(database_name)
+    create_table_setting(database_name)
+    create_table_family(database_name)
     create_table_customer(database_name)
     create_table_admin(database_name)
-    create_table_supervisor(database_name)
     create_table_loan(database_name)
+    create_table_installment(database_name)
     print('end creat database')
