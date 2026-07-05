b7e36a72ffe3e7b94711afe3dc564ba73c36184c
Arian <arianpanahi6@gmail.com>
2026-07-05 13:13:42 +0330

    add import_excel file to add excel data to database

--- DML.py
+++ DML.py
@@ -27,3 +27,58 @@ def add_admin_access1(admin_id , access_level=1):
     conn.close()
     return  cur.lastrowid
 
+def add_customer(family_id , full_name , amount ,is_active='true'):
+    print(full_name)
+    conn = mysql.connector.connect(**db_config, database=database_name)
+    cur = conn.cursor()
+    SQL_Query = "INSERT INTO CUSTOMER (FAMILY_ID,FULL_NAME,TOTAL_CAPITAL,IS_ACTIVE) VALUES (%s,%s,%s,%s);"
+    cur.execute(SQL_Query, (family_id , full_name , amount ,is_active))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return  cur.lastrowid
+
+
+def add_family():
+    conn = mysql.connector.connect(**db_config, database=database_name)
+    cur = conn.cursor()
+    SQL_Query = "INSERT INTO FAMILY (FAMILY_NAME) VALUES (%s);"
+    cur.execute(SQL_Query , (None ,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return  cur.lastrowid
+
+
+
+def add_family_data(family_id , head_id , family_name):
+    conn = mysql.connector.connect(**db_config, database=database_name)
+    cur = conn.cursor()
+    SQL_Query = "UPDATE FAMILY SET HEAD_ID=%s,FAMILY_NAME=%s WHERE ID=%s;"
+    cur.execute(SQL_Query, (head_id , family_name , family_id))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return  cur.lastrowid            
+
+
+def add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid):
+    conn = mysql.connector.connect(**db_config, database=database_name)
+    cur = conn.cursor()
+    SQL_Query = "INSERT INTO LOAN (CUSTOMER_ID,LOAN_AMOUNT,INSTALLMENT_AMOUNT,NUMBER_REMAINING_INSTALLMENTS,AMOUNT_PAID) VALUES (%s,%s,%s,%s,%s);"
+    cur.execute(SQL_Query , (customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return  cur.lastrowid 
+
+
+def add_installment(loan_id , NUMBER_PAID , Capital_increase):
+    conn = mysql.connector.connect(**db_config, database=database_name)
+    cur = conn.cursor()
+    SQL_Query = "INSERT INTO INSTALLMENT (LOAN_ID,INSTALLMENT_NUMBER,CAPITAL_INCREASE) VALUES (%s,%s,%s);"
+    cur.execute(SQL_Query , (loan_id , NUMBER_PAID , Capital_increase))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return  cur.lastrowid
---
+++ import_excel.py
@@ -0,0 +1,107 @@
+from DML import *
+import csv
+class Import_excel:
+    number = 0
+    family_data = {'family_id':None}
+    def __init__(self , line_number):
+        self.line_number = line_number
+    
+    def open_file(self):
+        file_datas = list()
+        with open('excel.csv' , 'r' , encoding='utf-8-sig') as f:
+            for num in range(self.line_number):
+                file = f.readline()
+                print(file)
+                file_list = file.split(',')
+                status = 'false'
+                if file_list[0] != '':
+                    print('file_list',file_list)
+                    status = 'true'
+                    file_data = {'full_name': file_list[-1].replace('\n','') + '' + file_list[-2],
+                            'loan_amount': None,
+                            'loan_number': None, 
+                            'capital_increase': None,
+                            'total_amount': None,
+                            'total_capital': None,
+                            'status':status}
+                elif status == 'false' and file_list[0] != '':
+                    file_data = {'full_name': file_list[-1].replace('\n','') + '' + file_list[-2],
+                                'loan_amount': int(file_list[-3]),
+                                'loan_number': int(file_list[-4]), 
+                                'capital_increase': int(file_list[-5]),
+                                'total_amount': int(file_list[-6]),
+                                'total_capital': int(file_list[-7]),
+                                'status':status}
+                file_datas.append(file_data)
+        return file_datas
+        
+
+    def add_customer_data(self , file_data  ,line_number):
+        print(file_data)
+        print(line_number)
+        if file_data['full_name'] == '':
+            self.number += 1
+            print('okk')
+        elif self.number == 1 or line_number==1:
+            print('ok')
+            family_id = add_family()
+            head_id = add_customer(family_id ,
+                                    file_data['full_name'] ,
+                                    file_data['total_capital'] ,
+                                    file_data['status'])
+            family_name = file_data['full_name']
+            add_family_data(family_id , int(head_id) , family_name)
+            self.family_data['family_id'] = family_id 
+            customer_id = head_id
+            installment_amount = file_data['loan_amount']
+            if  installment_amount :
+                loan_amount = 24 * installment_amount
+                number_paid_installment = 24-number_paid_installment
+            else:
+                loan_amount = None
+                number_paid_installment = None
+            number_paid_installment = file_data['loan_number']
+            amount_paid = file_data['total_amount']
+            Capital_increase = file_data['capital_increase']
+            loan_id = add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid)
+            add_installment(loan_id , number_paid_installment , Capital_increase)
+            self.number = 0
+        else:
+            family_id = self.family_data['family_id']
+            customer_id = add_customer(family_id , 
+                            file_data['full_name'] , 
+                            file_data['total_capital'] , 
+                            file_data['status'])
+            
+            installment_amount = file_data['loan_amount']
+            if  installment_amount :
+                loan_amount = 24 * installment_amount
+                number_paid_installment = 24-number_paid_installment
+            else:
+                loan_amount = None
+                number_paid_installment = None
+            number_paid_installment = file_data['loan_number']
+            amount_paid = file_data['total_amount']
+            Capital_increase = file_data['capital_increase']
+            loan_id = add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid)
+            add_installment(loan_id , number_paid_installment , Capital_increase)
+            print('ook')
+            
+
+
+
+        
+
+
+
+def main():
+    num = 0
+    im = Import_excel(8)
+    file_data = im.open_file()
+    print(file_data)
+    for file_data in file_data:
+        num += 1
+        im.add_customer_data(file_data  , num)
+
+if __name__ == "__main__":
+    main()
