ce7b3b65ec8828179b022f7f64109d45968245c9
Arian <arianpanahi6@gmail.com>
2026-07-05 14:22:26 +0330

    debug

--- import_excel.py
+++ import_excel.py
@@ -10,40 +10,53 @@ class Import_excel:
         file_datas = list()
         with open('excel.csv' , 'r' , encoding='utf-8-sig') as f:
             for num in range(self.line_number):
-                file = f.readline()
-                print(file)
-                file_list = file.split(',')
-                status = 'false'
-                if file_list[0] != '':
-                    print('file_list',file_list)
-                    status = 'true'
-                    file_data = {'full_name': file_list[-1].replace('\n','') + '' + file_list[-2],
-                            'loan_amount': None,
-                            'loan_number': None, 
-                            'capital_increase': None,
-                            'total_amount': None,
-                            'total_capital': None,
+                data = f.readline()
+                data_list = data.split(',')
+                # print(data_list)
+
+                if data_list[0] == 'new_line':
+                    file_data = {'full_name':'new_line'}
+                    # print(1)
+
+
+                elif data_list[0] == 'FALSE':
+                    status = 'false'
+                    # print(2)
+                    file_data = {'full_name': data_list[-2].replace('\n','') + ' ' + data_list[-1],
+                            'loan_amount': None ,
+                            'loan_number': None , 
+                            'capital_increase': None ,
+                            'total_amount': None ,
+                            'total_capital': None ,
                             'status':status}
-                elif status == 'false' and file_list[0] != '':
-                    file_data = {'full_name': file_list[-1].replace('\n','') + '' + file_list[-2],
-                                'loan_amount': int(file_list[-3]),
-                                'loan_number': int(file_list[-4]), 
-                                'capital_increase': int(file_list[-5]),
-                                'total_amount': int(file_list[-6]),
-                                'total_capital': int(file_list[-7]),
+                    
+                elif data_list[0] == 'TRUE':
+                    status = 'true'
+                    # print(3)
+                    file_data = {'full_name': data_list[-2].replace('\n','') + ' ' + data_list[-1],
+                                'loan_amount': int(data_list[-3]) ,
+                                'loan_number': int(data_list[-4]), 
+                                'capital_increase': int(data_list[-5]) ,
+                                'total_amount': int(data_list[-6]) ,
+                                'total_capital': int(data_list[-7]) ,
                                 'status':status}
+                    
+                
+
                 file_datas.append(file_data)
         return file_datas
         
 
     def add_customer_data(self , file_data  ,line_number):
-        print(file_data)
-        print(line_number)
-        if file_data['full_name'] == '':
+        # print(file_data)
+        # print(line_number)
+
+        if file_data['full_name'] == 'new_line':
+            # print(4,file_data['full_name'])
             self.number += 1
-            print('okk')
+
         elif self.number == 1 or line_number==1:
-            print('ok')
+            # print(5,file_data['full_name'])
             family_id = add_family()
             head_id = add_customer(family_id ,
                                     file_data['full_name'] ,
@@ -54,19 +67,23 @@ class Import_excel:
             self.family_data['family_id'] = family_id 
             customer_id = head_id
             installment_amount = file_data['loan_amount']
+            number_paid_installment = file_data['loan_number']
             if  installment_amount :
                 loan_amount = 24 * installment_amount
                 number_paid_installment = 24-number_paid_installment
             else:
                 loan_amount = None
                 number_paid_installment = None
-            number_paid_installment = file_data['loan_number']
             amount_paid = file_data['total_amount']
             Capital_increase = file_data['capital_increase']
-            loan_id = add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid)
-            add_installment(loan_id , number_paid_installment , Capital_increase)
+            if number_paid_installment is None:
+                pass
+            else:
+                loan_id = add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid)
+                add_installment(loan_id , number_paid_installment , Capital_increase)
             self.number = 0
         else:
+            # print(6,file_data['full_name'])
             family_id = self.family_data['family_id']
             customer_id = add_customer(family_id , 
                             file_data['full_name'] , 
@@ -74,18 +91,23 @@ class Import_excel:
                             file_data['status'])
             
             installment_amount = file_data['loan_amount']
+            number_paid_installment = file_data['loan_number']
             if  installment_amount :
                 loan_amount = 24 * installment_amount
-                number_paid_installment = 24-number_paid_installment
+                number_paid_installment = 24 - number_paid_installment
             else:
                 loan_amount = None
                 number_paid_installment = None
             number_paid_installment = file_data['loan_number']
             amount_paid = file_data['total_amount']
             Capital_increase = file_data['capital_increase']
-            loan_id = add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid)
-            add_installment(loan_id , number_paid_installment , Capital_increase)
-            print('ook')
+            print('number_paid_installment',number_paid_installment)
+            if number_paid_installment is None:
+                pass
+            else:
+                loan_id = add_loan_data(customer_id  , loan_amount , installment_amount , number_paid_installment , amount_paid)
+                add_installment(loan_id , number_paid_installment , Capital_increase)
+            # print('ook')
             
 
 
@@ -96,9 +118,9 @@ class Import_excel:
 
 def main():
     num = 0
-    im = Import_excel(8)
+    im = Import_excel(11)
     file_data = im.open_file()
-    print(file_data)
+    # print(file_data)
     for file_data in file_data:
         num += 1
         im.add_customer_data(file_data  , num)
